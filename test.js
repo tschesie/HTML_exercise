90f0d3bad2552935855addc0e47f9a451f6e01f6
Jessica Appiah <tschesi@jessicas-mbp.localdomain>
2020-11-17 22:40:31 +0100

    ProgÜb 20201117

---
+++ test.js
@@ -0,0 +1,16 @@
+function druckeVersandticketDruckerInJessicasZimmer(kunde, anschrift) {
+    console.log(kunde)
+    console.log(anschrift);
+}
+
+
+function berechneBetrag(einzelstückpreis, anzahl) {
+    console.log(einzelstückpreis + "€ x " + anzahl + " = " + einzelstückpreis*anzahl + "€")
+}
+
+function order(kunde, anschrift, einzelstückpreis, anzahl) {
+    druckeVersandticketDruckerInJessicasZimmer(kunde, anschrift)
+    berechneBetrag(einzelstückpreis, anzahl)
+}
+
+order("Philipp Hellwich", "Am Hölzlein 18", 12, 10)
