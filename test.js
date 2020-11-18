9654eed5823cb182277e8d791d9a77d7594155dd
Jessica Appiah <tschesi@jessicas-mbp.localdomain>
2020-11-18 22:11:09 +0100

    ..

--- test.js
+++ test.js
@@ -1,21 +1,24 @@
-function druckeVersandticketDruckerInJessicasZimmer(kunde, anschrift) {
-    console.log(kunde)
+function druckeVersandticketDruckerInJessicasZimmer(eins, anschrift) {
+    console.log(eins)
 }
 
 
 function berechneBetrag(einzelstückpreis, anzahl) {
-    console.log(einzelstückpreis + "€ x " + anzahl + " = " + einzelstückpreis*anzahl + "€")
-    druckeRechnung(einzelstückpreis, anzahl)
+    const betrag = einzelstückpreis * anzahl * 1.19
+    return betrag
 }
 
-function druckeRechnung(einzelstückpreis, anzahl) {
-    console.log(einzelstückpreis*anzahl + "€")
+function druckeRechnung(rechnungsbetrag) {
+    console.log(rechnungsbetrag + "€")
 }
 // wenn ich statt dem plus ein komma mache wird mir das Eurozeichen in der Konsole grün angezeigt und steht in Anführungszeichen: '€' - wieso und wie umgeh ich das?
 
 function order(kunde, anschrift, einzelstückpreis, anzahl) {
+    const hans = berechneBetrag(einzelstückpreis, anzahl)
+
     druckeVersandticketDruckerInJessicasZimmer(kunde, anschrift)
     berechneBetrag(einzelstückpreis, anzahl)
+    druckeRechnung(berechneterBetrag)
 }
 
 order("Philipp Hellwich", "Am Hölzlein 18", 12, 10)
@@ -23,3 +26,8 @@ order("Philipp Hellwich", "Am Hölzlein 18", 12, 10)
 
 
 // Implementiere eine neue Funktion druckeRechnung(), die mit dem errechneten Betrag aufgerufen wird. Diese soll auf der Konsole den berechneten Betraus ausgeben.
+
+
+function addiereneFunktion(parameter1, parameter2) {
+    return parameter1 + parameter2
+}
